e54381451199b8725e63979f01c66ebb85fdcb24
bricefriha <contact@bricefriha.dev>
2025-08-12 14:48:29 +0100

    Add project files.

---
+++ TokeroAssignment/Models/Token.cs
@@ -0,0 +1,9 @@
+﻿namespace TokeroAssignment.Models;
+
+public class Token
+{
+    public string Name { get; set; }
+    public string Symbol { get; set; }
+    public string Changes { get; set; }
+    public string PriceUsd { get; set; }
+}
---
+++ TokeroAssignment/ViewModels/HomeViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+using TokeroAssignment.Models;
+
+namespace TokeroAssignment.ViewModels;
+
+public class HomeViewModel : BaseViewModel
+{
+    private ObservableCollection<Token> _tokens;
+    public ObservableCollection<Token> Tokens
+    {
+        get 
+        { 
+            return _tokens; 
+        }
+        set
+        {
+            _tokens = value;
+            OnPropertyChanged(nameof(Tokens));
+        }
+    }
+    public HomeViewModel()
+    {
+        _tokens = new ObservableCollection<Token>()
+        {
+            new ()
+            {
+                Name = "Cardano",
+                Symbol= "ADA",
+            },
+            new ()
+            {
+                Name = "Cardano",
+                Symbol= "ADA",
+            }
+        };
+    }
+}
