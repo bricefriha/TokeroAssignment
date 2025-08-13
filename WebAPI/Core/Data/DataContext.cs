b1d599249d8d25b4baaa0ac26e08716ccd261174
bricefriha <contact@bricefriha.dev>
2025-08-13 11:12:52 +0100

    Setup webAPI

---
+++ WebAPI/Core/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Core.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options): base(options)
+    {
+
+    }
+
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<DcaSetup> Setups { get; set; }
+
+    
+}
