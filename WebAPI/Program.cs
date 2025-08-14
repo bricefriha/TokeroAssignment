fc8ed436b274607e77330ce529c7a9f75b8e354e
bricefriha <contact@bricefriha.dev>
2025-08-14 15:48:39 +0100

    server - prog | add json serializer options

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -6,7 +6,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+    options.JsonSerializerOptions.WriteIndented = true;
+});
+
 builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
