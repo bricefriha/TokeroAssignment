087df465d893699d04b5ea6d73b6e2da7f9bccc9
bricefriha <contact@bricefriha.dev>
2025-08-15 20:17:05 +0100

    WebApp | handle start date

--- TokeroAssignment/Models/DcaSetup.cs
+++ TokeroAssignment/Models/DcaSetup.cs
@@ -6,5 +6,19 @@ public class DcaSetup
 {
     public string UserDataId { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public double? AllocationAmountUSD { get; set; }
+
+    private int? _dayOfMonth;
+    public int DayOfMonth { get => _dayOfMonth?? CreatedAt.Day; set 
+        {
+            // Don't allow 0 as value
+            if (value == 0)
+            {
+                _dayOfMonth = CreatedAt.Day;
+                return;
+            }
+
+            _dayOfMonth = value;
+        } }
     public required ObservableCollection<TokenShare> Shares { get; set; }
 }
